6b415e6032e257641f030ace7861c79b2c4601e4
Emilio <barrera_emilio@yahoo.es>
2020-03-27 11:01:46 +0000

    Gif Loading

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -11,15 +11,16 @@ namespace EmployeeManagement.Web.Pages
     {
         public IEnumerable<Employee> Employees { get; set; }
 
-        protected override Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
 
-            LoadEmployees();
-            return base.OnInitializedAsync();
+           await Task.Run(LoadEmployees);
         }
 
         private void LoadEmployees()
         {
+
+            System.Threading.Thread.Sleep(3000);
 
             var employee1 = new Employee 
             {

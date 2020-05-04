9413bf4abfa98496fa9dcd15961972a3459a9774
Emilio <barrera_emilio@yahoo.es>
2020-05-04 15:25:53 +0100

    Blazor data binding

---
+++ EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemoBase  : ComponentBase
+    {
+        public string Name { get; set; } = "Emilio Barrera";
+        public string Gender { get; set; } = "Male";
+        public string Colour { get; set; } = "background-color:red";
+    }
+}
